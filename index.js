f96e88830e4910e4ddae5b7558dbaf19e058a7f5
Luca <pereluca10@gmail.com>
2021-03-04 12:08:10 -0500

    Added procfile

--- index.js
+++ index.js
@@ -13,13 +13,11 @@ client.on('message', msg => {
     const word = '*';
     const userID = process.env.ID;
 
-    if(msg.author.id === userID) {
-        if(msg.content.includes(word)) {
+    if(msg.author.id === userID && msg.content.includes(word)) {
             msg.delete();
             msg.channel.send(uwu(msg.content).split(word)); 
-        }
     }
     else return;
-});
+}); 
 
 client.login(process.env.TOKEN);  
