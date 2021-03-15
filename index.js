28c643019ae9de7669ddbc27d3c0a12243feb8d6
luca <pereluca10@gmail.com>
2021-03-15 04:42:33 -0400

    Added new env variable PREFIX, added chalk, formatted with Prettier

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const Discord = require('discord.js');
 const uwu = require('uwu-js');
+const chalk = require('chalk');
 require('dotenv').config();
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
+	console.log(chalk.green.bold(`Logged in as ${client.user.tag}!`));
 });
 
-client.on('message', msg => {
-    const word = '*';
-    const userID = process.env.ID;
+client.on('message', (msg) => {
+	const word = process.env.PREFIX;
+	const userID = process.env.ID;
 
-    if(msg.author.id === userID && msg.content.includes(word)) {
-            msg.delete();
-            msg.channel.send(uwu(msg.content).split(word)); 
-    }
-    else return;
-}); 
+	if (msg.author.id === userID && msg.content.includes(word)) {
+		try {
+			msg.delete();
+			msg.channel.send(uwu(msg.content).split(word));
+		} catch {
+			console.log(chalk.red.bold('Something went wrong when deleting'));
+		}
+	} else return;
+});
 
-client.login(process.env.TOKEN);  
+client.login(process.env.TOKEN);
